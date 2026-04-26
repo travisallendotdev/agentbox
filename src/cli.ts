44fdb62d33488bb61285b23071257d5e679e0838
Travis Allen <travisallen6@gmail.com>
2026-04-25 20:18:05 -0600

    chore: initialize agentbox Bun project

---
+++ src/cli.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env bun
+console.log("agentbox v0.1.0");
+process.exit(0);
---
+++ tests/unit/smoke.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "bun:test";
+
+test("smoke", () => {
+  expect(1 + 1).toBe(2);
+});
